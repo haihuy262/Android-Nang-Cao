070f769e128ea93a8a0e456e8273996784227a34
haihuy262 <dolcegabana0206@gmail.com>
2023-05-05 08:02:22 +0700

    Add Lab-1 Lab-2

---
+++ Lab-2/app/src/main/java/huynhph30022/fpoly/lab2/MainActivity.java
@@ -0,0 +1,14 @@
+package huynhph30022.fpoly.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
