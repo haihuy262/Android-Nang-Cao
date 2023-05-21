95b4996eb86c297c41895de443cf302a53b38b44
haihuy262 <dolcegabana0206@gmail.com>
2023-05-21 15:27:37 +0700

    New Service

--- Bai-Tap-Nop/Music/app/src/main/java/huynhph30022/fpoly/music/service/MusicService.java
+++ Bai-Tap-Nop/Music/app/src/main/java/huynhph30022/fpoly/music/service/MusicService.java
@@ -13,10 +13,10 @@ import androidx.annotation.Nullable;
 import java.io.IOException;
 
 public class MusicService extends Service {
-    MediaPlayer mediaPlayer;
-    String currentMusicUri;
-    IBinder iBinder = new MusicBinder();
-    String musicUri;
+    private MediaPlayer mediaPlayer;
+    private String currentMusicUri;
+    private boolean isPaused = false;
+    private final IBinder binder = new MusicBinder();
 
     public class MusicBinder extends Binder {
         MusicService getService() {
@@ -32,15 +32,57 @@ public class MusicService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        musicUri = intent.getStringExtra("musicUri");
-        
-        return START_STICKY;
+        String musicUri = intent.getStringExtra("musicUri");
+        if (musicUri != null) {
+            playMusic(musicUri);
+        }
+        return START_NOT_STICKY;
     }
 
+    public void playMusic(String musicUri) {
+        try {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.stop();
+            }
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(musicUri);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+            currentMusicUri = musicUri;
+            isPaused = false;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pauseMusic() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            isPaused = true;
+        }
+    }
+
+    public void resumeMusic() {
+        if (isPaused) {
+            mediaPlayer.start();
+            isPaused = false;
+        }
+    }
+
+    public void stopMusic() {
+        mediaPlayer.stop();
+        mediaPlayer.reset();
+        currentMusicUri = null;
+        isPaused = false;
+    }
+
+    public String getCurrentMusicUri() {
+        return currentMusicUri;
+    }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return binder;
     }
 }
