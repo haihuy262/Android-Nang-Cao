2731149e325dc23da4d549f60584c2171f79d336
haihuy262 <dolcegabana0206@gmail.com>
2023-06-12 14:22:35 +0700

    Music Total Time And Current Time

--- Bai-Tap-Nop/Assignment-MOB201/app/src/main/java/huynhph30022/fpoly/assignmentmob201/fragment/MusicFragment.java
+++ Bai-Tap-Nop/Assignment-MOB201/app/src/main/java/huynhph30022/fpoly/assignmentmob201/fragment/MusicFragment.java
@@ -1,5 +1,10 @@
 package huynhph30022.fpoly.assignmentmob201.fragment;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,23 +15,77 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 import huynhph30022.fpoly.assignmentmob201.R;
 import huynhph30022.fpoly.assignmentmob201.adapter.MusicAdapter;
 import huynhph30022.fpoly.assignmentmob201.dao.MusicDAO;
 import huynhph30022.fpoly.assignmentmob201.model.Song;
+import huynhph30022.fpoly.assignmentmob201.service.MusicService;
 
 public class MusicFragment extends Fragment {
     protected RecyclerView recyclerViewMusic;
     protected MusicDAO musicDAO;
     protected ArrayList<Song> list;
     protected MusicAdapter adapter;
-    protected TextView tvTitleBaiHat;
+    protected TextView tvTitleBaiHat, tvCurrentTime, tvTotalTime;
     protected RelativeLayout relativeLayoutControlMusic;
     protected LinearLayoutManager layoutManager;
+    protected SeekBar seekBar;
+    protected MediaPlayer mediaPlayer;
+    protected BroadcastReceiver updateSeekBarReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int totalTime = intent.getIntExtra("totalTime", 0);
+            updateSeekBar(totalTime);
+        }
+    };
+    protected BroadcastReceiver updateUIReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int currentTime = intent.getIntExtra("currentTime", 0);
+            updateUI(currentTime);
+        }
+    };
+
+    private void updateUI(int currentTime) {
+        seekBar.setProgress(currentTime);
+        tvCurrentTime.setText(formatTime(currentTime));
+    }
+
+    private String formatTime(int timeInMillis) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("mm:ss", Locale.getDefault());
+        return dateFormat.format(new Date(timeInMillis));
+    }
+
+    private void updateSeekBar(int totalTime) {
+        seekBar.setMax(totalTime);
+        tvTotalTime.setText(formatTime(totalTime));
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -35,6 +94,9 @@ public class MusicFragment extends Fragment {
         recyclerViewMusic = view.findViewById(R.id.recyclerMusic);
         tvTitleBaiHat = view.findViewById(R.id.song_title);
         relativeLayoutControlMusic = view.findViewById(R.id.controlMusic);
+        seekBar = view.findViewById(R.id.seekBar);
+        tvCurrentTime = view.findViewById(R.id.current_time);
+        tvTotalTime = view.findViewById(R.id.total_time);
 
         musicDAO = new MusicDAO(requireContext());
         list = new ArrayList<>();
@@ -46,5 +108,19 @@ public class MusicFragment extends Fragment {
         adapter.setData(list);
         recyclerViewMusic.setAdapter(adapter);
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        requireActivity().registerReceiver(updateSeekBarReceiver, new IntentFilter(MusicService.ACTION_UPDATE_SEEKBAR));
+        requireActivity().registerReceiver(updateUIReceiver, new IntentFilter(MusicService.ACTION_UPDATE_UI));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        requireActivity().unregisterReceiver(updateSeekBarReceiver);
+        requireActivity().unregisterReceiver(updateUIReceiver);
     }
 }
--- Bai-Tap-Nop/Assignment-MOB201/app/src/main/java/huynhph30022/fpoly/assignmentmob201/service/MusicService.java
+++ Bai-Tap-Nop/Assignment-MOB201/app/src/main/java/huynhph30022/fpoly/assignmentmob201/service/MusicService.java
@@ -2,11 +2,25 @@ package huynhph30022.fpoly.assignmentmob201.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Handler;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 
+import java.io.IOException;
+
 public class MusicService extends Service {
+    public static final String ACTION_UPDATE_SEEKBAR = "huynhph30022.fpoly.UPDATE_SEEK_BAR";
+    public static final String ACTION_UPDATE_UI = "huynhph30022.fpoly.UPDATE_UI";
+    protected int totalTime;
+    protected int currentTime;
+    private MediaPlayer mediaPlayer;
+    private Handler handler;
+    private Runnable runnable;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -15,8 +29,56 @@ public class MusicService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        String musicUri = intent.getStringExtra("link");
+        if (intent != null) {
+            String musicUri = intent.getStringExtra("link");
+            playMusic(musicUri);
+        }
         return START_NOT_STICKY;
+    }
+
+    private void playMusic(String musicUri) {
+        Uri uriMusic = Uri.parse(musicUri);
+        mediaPlayer = new MediaPlayer();
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build();
+        mediaPlayer.setAudioAttributes(audioAttributes);
+        try {
+            mediaPlayer.setDataSource(getApplicationContext(), uriMusic);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+            totalTime = mediaPlayer.getDuration();
+            updateSeekBar(totalTime);
+
+            handler = new Handler();
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    if (mediaPlayer != null) {
+                        currentTime = mediaPlayer.getCurrentPosition();
+                        updateUI(currentTime);
+                        handler.postDelayed(this, 1000);
+                    }
+                }
+            };
+            handler.postDelayed(runnable, 1000);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void updateUI(int currentTime) {
+        Intent intent = new Intent(ACTION_UPDATE_UI);
+        intent.putExtra("currentTime", currentTime);
+        sendBroadcast(intent);
+    }
+
+    private void updateSeekBar(int totalTime) {
+        Intent intent = new Intent(ACTION_UPDATE_SEEKBAR);
+        intent.putExtra("totalTime", totalTime);
+        sendBroadcast(intent);
     }
 
     @Override
